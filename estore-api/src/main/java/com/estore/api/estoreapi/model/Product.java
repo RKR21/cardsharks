21887c15c4b7dc45e7d841253567e42d748e2a9b
ElijahLenhard <elijahlenhard@gmail.com>
2023-02-14 13:58:27 -0500

    documents the Product class

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Product.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Product.java
@@ -2,9 +2,13 @@ package com.estore.api.estoreapi.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Represents a product
+ * 
+ * 
+ * @author Elijah Lenhard
+ */
 public class Product {
-
-    
     static final String STRING_FORMAT = "Product [id=%d, name=%s, price=%f]";
 
     @JsonProperty("id") private int id;
@@ -14,8 +18,9 @@ public class Product {
     
     /**
      * Create a new Product object
-     * 
-     * 
+     * @Param id Unique int for the product
+     * @Param name Display name of the product
+     * @Param price Listed price of the product
      * 
      */
     public Product(@JsonProperty("id") int id, @JsonProperty("name") String name, 
@@ -24,19 +29,42 @@ public class Product {
         this.name = name;
         this.price = price;
     }
-
+    /**
+     * Retrives the id of the product
+     * @return Id of the product
+     */
     public int getId() {
         return this.id;
     }
+
+    /**
+     * Retrives the name of the product
+     * @return name of product
+     */
     public String getName(){
         return this.name;
     }
+
+    /**
+     * Updates the name of the product
+     * @param String new name for product
+     */
     public void setName(String name){
         this.name = name;
     }
+
+    /**
+     * Retrives the price of the product
+     * @return price of the product
+     */
     public double getPrice(){
         return this.price;
     }
+
+    /**
+     * Updates the price of the product
+     * @param double new price for product
+     */
     public void setPrice(double price){
         this.price = price;
     }
