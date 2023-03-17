2916d1a9dad5536e06d1de2847f1436863c4db18
Adrian Marcellus <alm8252@g.rit.edu>
2023-03-17 15:37:32 -0400

    Fixed user status retention

--- estore-ui/src/app/account.service.ts
+++ estore-ui/src/app/account.service.ts
@@ -12,6 +12,10 @@ export class AccountService {
   private accountUrl = 'http://localhost:8080/account';  // URL to web api
   private cartUrl = 'http://localhost:8080/cart'
 
+  private static token = 0;
+  private static userName = '';
+  private static logStatus = false;
+
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
@@ -22,4 +26,28 @@ export class AccountService {
     const url = `${this.accountUrl}/?userName=${user}`;
     return this.http.get<Token>(url);
   }
+
+  static setToken(value : number){
+    this.token = value;
+  }
+  static getToken() {
+    return this.token;
+  }
+
+  static setUser(value : string){
+    this.userName = value;
+  }
+
+  static getUser() {
+    return this.userName;
+  }
+
+  static setLogStatus(value : boolean){
+    this.logStatus = value;
+  }
+
+  static isLoggedIn() {
+    return this.logStatus;
+  }
+
 }
