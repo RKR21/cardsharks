668a047257b83aae0f30adfa63610f812ffebb32
ElijahLenhard <elijahlenhard@gmail.com>
2023-03-17 15:55:19 -0400

    Finished account integration.

--- estore-ui/src/app/app.component.ts
+++ estore-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AccountService } from './account.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+
+  isAdmin(){
+    return (AccountService.getToken()==92668751);
+  }
+
 }
+
