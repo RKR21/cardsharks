67d93b4a01ede81e3dcd9acbe6aeef98945bbb68
RKR21 <raofm@icloud.com>
2023-03-19 13:16:13 -0400

    Search bar shows no products found when search term does not match any product

--- estore-ui/src/app/product-search/product-search.component.ts
+++ estore-ui/src/app/product-search/product-search.component.ts
@@ -1,42 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Observable, of, Subject } from 'rxjs';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
+
 @Component({
   selector: 'app-product-search',
   templateUrl: './product-search.component.html',
   styleUrls: ['./product-search.component.css']
 })
-export class ProductSearchComponent implements OnInit{
-  products$: Product[] = [];
-  private searchTerms = new Subject<string>();
-  private previousTerm = '';
+export class ProductSearchComponent implements OnInit {
+  public term: string = '';
+  products: Product[] = [];
 
   constructor(private productService: ProductService){}
 
-  // push a term onto the observable stream
   search(term:string): void {
-    //console.log('search term:', term);
-    this.searchTerms.next(term)
+    this.term = term.trim();
+    this.productService.searchProducts(term).subscribe(products => {
+      this.products = products;
+    });
   }
-  
 
   ngOnInit(): void {
-    this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
-      debounceTime(300),
-      // ignore new term if same as previous term
-      distinctUntilChanged(),
-      // switch to new search observable each time the term changes
-      switchMap((term: string) => this.productService.searchProducts(term))
-    )
-    .subscribe(products => this.products$ = products);
+    
   }
-  
-
-
-
 }
--- estore-ui/src/app/product.service.ts
+++ estore-ui/src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, Observable, of, switchMap } from 'rxjs';
 import { Product } from './product';
 
 @Injectable({
@@ -12,23 +12,47 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]>{
+    console.log("It is being called")
     return this.http.get<Product[]>(this.productsUrl);
   }
 
   getProduct(id: number): Observable<Product>{
+    
     const url = `${this.productsUrl}/${id}`;
     return this.http.get<Product>(url);
   }
 
   searchProducts(term: string): Observable<Product[]> {
+    
     if (!term.trim()) {
       return of([]);
     }
 
-    //const url = `${this.productsUrl}/search?name=${term}`;
-    
-    //const url = `${this.productsUrl}/search?name=${term}&t=${new Date().getTime()}`;
     const url = `${this.productsUrl}/search?name=${term}`;
-    return this.http.get<Product[]>(url);
+    
+    
+    
+    return this.http.get<Product[]>(url).pipe(
+      //console.log("products length =" + products.length)
+      map(products => {
+        console.log("products length =" + products.length)
+        if (products.length === 0) {
+          console.log("products length =" + products.length)
+          return [];
+        } else {
+          console.log("products length =" + products.length)
+          return products;
+        }
+      }),
+      catchError(error => {
+        console.log("Error occurred:", error);
+        return of([]);
+      })
+    );
+    
+    
   }
+  
+  
+  
 }
