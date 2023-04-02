b1edb8684c67da4f90fa383dede0439f5d0b86ea
ElijahLenhard <elijahlenhard@gmail.com>
2023-04-01 20:40:16 -0400

    Finished test cases for collectionFileDAO Finished test cases for AccountFileDAO related to payment methods

--- estore-api/src/test/java/com/estore/api/estoreapi/persistance/CollectionFileDaoTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistance/CollectionFileDaoTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -31,16 +32,16 @@ public class CollectionFileDaoTest {
         testCollections = new Collection[3];
         testCollections[0] = new Collection("User1");
 
-        testCollections[0].addToCollection(new Product(1, "test", 8.4, 3));
-        testCollections[0].addToCollection(new Product(2, "test2", 4.8, 4));
+        testCollections[0].addToCollection(new Product(1, "test", 8.4, 1));
+        testCollections[0].addToCollection(new Product(2, "test2", 4.8, 1));
         testCollections[1] = new Collection("User2");
 
-        testCollections[1].addToCollection(new Product(2, "test2", 4.8, 4));
+        testCollections[1].addToCollection(new Product(3, "test3", 4.8, 1));
         testCollections[2] = new Collection("User3");
         when(mockObjectMapper
             .readValue(new File("doesnt_matter.txt"), Collection[].class))
                 .thenReturn(testCollections);
-        collectionFileDAO = new CollectionFileDAO("Doesn't matter", mockObjectMapper);
+        collectionFileDAO = new CollectionFileDAO("doesnt_matter.txt", mockObjectMapper);
     }
 
     @Test
@@ -67,7 +68,7 @@ public class CollectionFileDaoTest {
     @Test
     public void testAddToCollection() throws IOException{
         Product toAdd = new Product(4, "New Product", 1000, 1);
-        String userName = "user1";
+        String userName = "User1";
         int token = Account.getToken(userName);
         //Product added = assertDoesNotThrow(()-> collectionFileDAO.addToCollection(token, toAdd));
         Product added = collectionFileDAO.addToCollection(token, toAdd);
@@ -86,7 +87,7 @@ public class CollectionFileDaoTest {
 
     @Test
     public void testRemoveFromCollection() throws IOException{
-        String userName = "user1";
+        String userName = "User1";
         int token = Account.getToken(userName);
         int id = 1;
         
@@ -117,19 +118,78 @@ public class CollectionFileDaoTest {
 
     @Test
     public void testMakeOffer() throws IOException{
-        String user1 = "user1";
-        String user2 = "user2";
+        String user1 = "User1";
+        String user2 = "User2";
         int token = Account.getToken(user1);
-        Product offer = new Product(1, "Product", 1000, 100);
-        Product request = new Product(2, "Proudct2", 100000, 23);
+        int offerToken = Account.getToken(user2);
+        Product offer = new Product(1, "test", 8.4, 1);
+        Product request = new Product(3, "test3", 4.8, 1);
 
         Trade result = assertDoesNotThrow(()->collectionFileDAO.makeOffer(token, user1, user2, request, offer),
                                      "Unexpected Exception thrown");
-        
+        Trade actual = collectionFileDAO.getOffer(offerToken);
         assertNotNull(result);
+        assertEquals(result, actual);
+    }
 
+    @Test
+    public void testAcceptOffer() throws IOException{
+        //setting up offer
+        String user1 = "User1";
+        String user2 = "User2";
+        int token = Account.getToken(user1);
+        int offerToken = Account.getToken(user2);
+        Product offer = new Product(1, "test", 8.4, 1);
+        Product request = new Product(3, "test3", 4.8, 1);
+        collectionFileDAO.makeOffer(token, user1, user2, request, offer);
+
+
+
+        boolean result = assertDoesNotThrow(()->collectionFileDAO.acceptOffer(offerToken), "Unexpected exception thrown");
+        assertEquals(true, result);
+        Product[] user1Col = collectionFileDAO.getCollection(Account.getToken(user1));
+        Product[] user2Col = collectionFileDAO.getCollection(Account.getToken(user2));
+        boolean user1HasRequest = false;
+        boolean user2HasOffer = false;
+
+
+        //Checking if products changed hands
+        for(Product p:user1Col){
+            if(p.equals(request)){
+                user1HasRequest = true;
+            }
+        }
+        for(Product p:user2Col){
+            if(p.equals(offer)){
+                user2HasOffer = true;
+            }
+        }
+        
+        assert(user1HasRequest);
+        assert(user2HasOffer);
 
     }
 
+    @Test
+    public void testRejectOffer() throws IOException{
+        //Setting up offer
+        String user1 = "User1";
+        String user2 = "User2";
+        int token = Account.getToken(user1);
+        int offerToken = Account.getToken(user2);
+        Product offer = new Product(1, "test", 8.4, 1);
+        Product request = new Product(3, "test3", 4.8, 1);
+
+        collectionFileDAO.makeOffer(token, user1, user2, request, offer);
+
+        
+        boolean success = assertDoesNotThrow(()->collectionFileDAO.rejectOffer(offerToken), "Unexpected exception thrown");
+        assert(success);
+
+        Trade actual = collectionFileDAO.getOffer(token);
+        assertNull(actual);
+    }
+
+    
     
 }
